59bc6f01ca7b393abdf54bff34f60ecaed59951f
BrandeenoLee <47995527+BrandeenoLee@users.noreply.github.com>
2020-09-18 15:39:23 -0500

    Create unusedID.js

---
+++ unusedID.js
@@ -0,0 +1,19 @@
+function nextId(ids){
+  const used = new Set(ids);
+  for (let i = 0; i <= ids.length; i++) {
+    if (!used.has(i)) return i;
+  }
+}
+
+//function nextId(ids){
+  var x = 0;
+  while (ids.includes(x)) x++;
+  return x;
+}
+
+//function nextId(ids){
+  for (var i=0;i<ids.length;i++){
+    if (ids.indexOf(i)==-1) return i;
+  }
+  return ids.length;
+}
