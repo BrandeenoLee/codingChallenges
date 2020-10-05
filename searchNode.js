92eba9b9b0311af5146e2a69feb37e3b8da59579
BrandeenoLee <47995527+BrandeenoLee@users.noreply.github.com>
2020-10-05 18:15:18 -0500

    Create searchNode.js

---
+++ searchNode.js
@@ -0,0 +1,19 @@
+SinglyList.prototype.searchNodeAt = function(position) {
+    var currentNode = this.head,
+        length = this._length,
+        count = 1,
+        message = {failure: 'Failure: non-existent node in this list.'};
+ 
+    // 1st use-case: an invalid position 
+    if (length === 0 || position < 1 || position > length) {
+        throw new Error(message.failure);
+    }
+ 
+    // 2nd use-case: a valid position 
+    while (count < position) {
+        currentNode = currentNode.next;
+        count++;
+    }
+ 
+    return currentNode;
+};
