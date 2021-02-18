b49822ace2759b1fd5a5b5d3b3aeb93768131b61
BrandeenoLee <brandonladcock@gmail.com>
2021-02-18 16:57:00 -0600

    completed problem.

---
+++ makeChangeRecursion.js
@@ -0,0 +1,12 @@
+var makeChange = function(total) {
+
+    var coins = [1,2,5,10,20,50,100,200];
+    var count = 0;
+   
+    var changer = function() {
+      // our recursive function
+    };
+  
+    changer();
+    return count;
+  };
